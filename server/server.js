a4347028c8c31307ecf8e61d079780524bc3d792
Angela Lee <angela.tylee25@gmail.com>
2025-12-03 23:04:19 +0800

    Configure server for Render deployment

--- server/server.js
+++ server/server.js
@@ -62,4 +62,5 @@ app.post('/', async(req, res) => {
   }
 })
 
-app.listen(3001, () => console.log('Server is running on port http://localhost:3001'))
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
